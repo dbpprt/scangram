95b3cc2416a313e5f4089e4c652fce96d5f54292
Dennis Bappert <bappert@outlook.com>
2017-09-25 18:57:51 +0200

    initial commit

---
+++ src/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Scangram.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

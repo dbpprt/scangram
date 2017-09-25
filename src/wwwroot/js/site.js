b2367662bf5c59809f7496a38e5d099fdbda9000
Santina Pianu <santina.pianu@googlemail.com>
2017-09-25 22:04:22 +0200

    Interact js with dummy image

--- src/wwwroot/js/site.js
+++ src/wwwroot/js/site.js
@@ -1,1 +1,35 @@
-﻿// Write your JavaScript code.
+﻿document.addEventListener('DOMContentLoaded', function () {
+
+    interact('.scanagram-cropperpoint')
+        .draggable({
+            // enable inertial throwing
+            inertia: true,
+            // keep the element within the area of it's parent
+            restrict: {
+                restriction: document.getElementsByClassName('scanagram-image')[0],
+                endOnly: true,
+                elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+            },
+            // enable autoScroll
+            autoScroll: true,
+            // call this function on every dragmove event
+            onmove: dragMoveListener,
+        });
+
+});
+
+function dragMoveListener(event) {
+    var target = event.target,
+        // keep the dragged position in the data-x/data-y attributes
+        x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+        y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+    // translate the element
+    target.style.webkitTransform =
+        target.style.transform =
+        'translate(' + x + 'px, ' + y + 'px)';
+
+    // update the posiion attributes
+    target.setAttribute('data-x', x);
+    target.setAttribute('data-y', y);
+}
